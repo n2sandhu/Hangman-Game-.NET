245a7248315c90872e172635b53de560875d660f
navisandhu2013 <navi_sandhu@yahoo.com.au>
2014-12-11 01:19:43 +1100

    added necessary files

---
+++ Default.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" Debug="true" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script language="javascript" type="text/javascript">
+
+    </script>
+    <link rel="Stylesheet" href="StyleSheet.css" />
+</head>
+<body style="height: 335px">
+    <form id="Form1" runat="server">
+  <p><b> <h1>HANGMAN GAME</h1></b></p>
+  <p>
+       
+    <asp:Button CssClass="butt" ID="A" runat="server" onclick="LetterGuessed" Text="A" />
+    <asp:Button CssClass="butt" ID="B" runat="server" onclick="LetterGuessed" Text="B" />
+    <asp:Button CssClass="butt" ID="C" runat="server" onclick="LetterGuessed" Text="C" />
+    <asp:Button CssClass="butt" ID="D" runat="server" onclick="LetterGuessed" Text="D" />
+    <asp:Button CssClass="butt" ID="E" runat="server" onclick="LetterGuessed" Text="E" />
+    <asp:Button CssClass="butt" ID="F" runat="server" onclick="LetterGuessed" Text="F" />
+    <asp:Button CssClass="butt" ID="G" runat="server" onclick="LetterGuessed" Text="G" />
+    <asp:Button CssClass="butt" ID="H" runat="server" onclick="LetterGuessed" Text="H" />
+    <asp:Button CssClass="butt" ID="I" runat="server" onclick="LetterGuessed" Text="I" />
+    <asp:Button CssClass="butt" ID="J" runat="server" onclick="LetterGuessed" Text="J" />
+    <asp:Button CssClass="butt" ID="K" runat="server" onclick="LetterGuessed" Text="K" />
+    <asp:Button CssClass="butt" ID="L" runat="server" onclick="LetterGuessed" Text="L" />
+    <asp:Button CssClass="butt" ID="M" runat="server" onclick="LetterGuessed" Text="M" />
+    <asp:Button CssClass="butt" ID="N" runat="server" onclick="LetterGuessed" Text="N" />
+    <asp:Button CssClass="butt" ID="O" runat="server" onclick="LetterGuessed" Text="O" />
+    <asp:Button CssClass="butt" ID="P" runat="server" onclick="LetterGuessed" Text="P" />
+    <asp:Button CssClass="butt" ID="Q" runat="server" onclick="LetterGuessed" Text="Q" />
+    <asp:Button CssClass="butt" ID="R" runat="server" onclick="LetterGuessed" Text="R" />
+    <asp:Button CssClass="butt" ID="S" runat="server" onclick="LetterGuessed" Text="S" />
+    <asp:Button CssClass="butt" ID="T" runat="server" onclick="LetterGuessed" Text="T" />
+    <asp:Button CssClass="butt" ID="U" runat="server" onclick="LetterGuessed" Text="U" />
+    <asp:Button CssClass="butt" ID="V" runat="server" onclick="LetterGuessed" Text="V" />
+    <asp:Button CssClass="butt" ID="W" runat="server" onclick="LetterGuessed" Text="W" />
+    <asp:Button CssClass="butt" ID="X" runat="server" onclick="LetterGuessed" Text="X" />
+    <asp:Button CssClass="butt" ID="Y" runat="server" onclick="LetterGuessed" Text="Y" />
+    <asp:Button CssClass="butt" ID="Z" runat="server" onclick="LetterGuessed" Text="Z" />
+  </p>
+
+    <asp:Button ID="Button1" runat="server" Text="Start a new Game" onclick="Button1_Click" />
+ 
+    <p>
+        
+    <asp:Label ID="Label1" runat="server" Text="Label" CssClass="word"></asp:Label>
+ 
+    <p>
+        
+    <asp:Label ID="Label2" runat="server" CssClass="attempts">Number of Attempts Left :</asp:Label>
+    <asp:Label ID="Label3" runat="server" CssClass="word"></asp:Label>
+    <p>
+    <asp:Label ID="Label4" runat="server" CssClass="word"></asp:Label>
+    </p>
+
+ 
+</form>
+    </body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Collections;
+
+using System.Text.RegularExpressions;
+using System.Drawing;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string word;
+    string maskedWord;
+    int attemptCount = 5;
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {            
+            game();
+        }
+        else {
+           word = (string)ViewState["word"];
+           maskedWord = (string)ViewState["maskedWord"];
+           attemptCount = (int)ViewState["attemptCount"];            
+        }
+    }
+
+    protected void Page_PreRender(Object sender, EventArgs e)
+    {
+        ViewState["word"] = word;
+        ViewState["maskedWord"] = maskedWord;
+        ViewState["attemptCount"] = attemptCount;
+    }
+
+
+    protected void game()
+    {
+        A.Enabled = true;        B.Enabled = true;        C.Enabled = true;        D.Enabled = true;        E.Enabled = true;
+        F.Enabled = true;        G.Enabled = true;        H.Enabled = true;        I.Enabled = true;        J.Enabled = true;
+        K.Enabled = true;        L.Enabled = true;        M.Enabled = true;        N.Enabled = true;        O.Enabled = true;
+        P.Enabled = true;        Q.Enabled = true;        R.Enabled = true;        S.Enabled = true;        T.Enabled = true;
+        U.Enabled = true;        V.Enabled = true;        W.Enabled = true;        X.Enabled = true;        Y.Enabled = true;
+        Z.Enabled = true;        Button1.Enabled = false;
+        attemptCount = 5;
+        Label2.Text = "Number Of attempts Left: ";
+        ArrayList words = new ArrayList();
+        Random random = new Random();
+        try
+        {
+            Array userData = File.ReadAllLines(Server.MapPath("TextFile.txt"));
+            foreach (string dataLine in userData) 
+            {
+                dataLine.ToString();
+                words.Add(dataLine);
+            }
+            string[] wordss = words.ToArray(typeof(string)) as string[];
+            word = wordss[random.Next(wordss.Length)];
+            maskedWord = word;
+            maskedWord = Regex.Replace(word, "[A-z]", "-");
+            Label1.Text = maskedWord;
+            Label3.Text = attemptCount.ToString();
+            Label4.Text = "";
+           
+        }
+        catch (Exception ex)
+        {
+            Label1.Text = "Cannot read the file "+ ex;
+        }
+       
+    }
+    protected void LetterGuessed(object sender, EventArgs e)
+    {
+
+        if (attemptCount != 0 && (Label4.Text.Equals("")))
+        {
+            string letter = ((Control)sender).ID.ToString().ToLower();
+            Button b1 = (Button)sender;
+            b1.Enabled = false;
+            int startPosition = word.IndexOf(letter);
+            if (!(startPosition == -1))
+            {
+                do
+                {
+                    maskedWord = maskedWord.Remove(startPosition, 1).Insert(startPosition, letter);
+                    Label1.Text = maskedWord;
+                    startPosition = word.IndexOf(letter, startPosition + 1);
+                }
+                while (startPosition != -1);
+            }
+            else
+            {
+                attemptCount--;
+                Label3.Text = attemptCount.ToString();
+            }
+            if (maskedWord.Equals(word))
+            {
+                Label1.Text = word;
+                Label4.Text = "You Win";
+                Button1.Enabled = true;
+                Label2.Text = "You won the game with " + (attemptCount).ToString() + " attempts left";
+                Label3.Text = ""; 
+            }
+
+            if (attemptCount == 0 && !(maskedWord.Equals(word)))
+            {
+                Label4.Text = "You Lost";
+                Label1.Text = word;
+                Button1.Enabled = true;
+                Label2.Text = "No more attempts left";
+                Label3.Text = "";
+            }
+
+        }
+        else
+        { 
+            //Do nothing on letter button click
+        }
+        
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        game();
+    }
+}
